bd2b718b1edffc2b7ce6eefe5b86d9924bd010c7
Vincent Brunet-Dupont <vbrunetdupont@gmail.com>
2017-07-07 21:19:11 -0400

    wwise project

--- Assets/_Script/HandMoving.cs
+++ Assets/_Script/HandMoving.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HandMoving : MonoBehaviour {
-
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
