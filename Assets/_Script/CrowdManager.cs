94d05787858a91ee33361cb163f7c82ec437155b
Alexandre <apernotlopes@github.com>
2017-07-08 11:43:37 -0400

    npc basic state machine implementation / alpha model

--- Assets/_Script/CrowdManager.cs
+++ Assets/_Script/CrowdManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using System.Linq;
 
 public class CrowdManager : MonoBehaviour 
 {
@@ -10,15 +11,51 @@ public class CrowdManager : MonoBehaviour
 	public Item[] items;
 
 	private List<Npc> _pooledNPCs;
-	
-	private IEnumerator SpawnNPCs(int number = 6) // WOLOLO
-	{
-		yield return 0;
 
+	//DEBUG
+	public Transform spawn;
+
+	private void Start()
+	{
+		_pooledNPCs = new List<Npc>();
+		StartCoroutine(SpawnNPCs(spawn.position, 12));
+	}
+	
+	private IEnumerator SpawnNPCs(Vector3 basePos, int number = 6) // WOLOLO
+	{
 		for (int i = 0; i < number; i++)
 		{
+			if(_pooledNPCs.Count == 0)
+			{
+				NewNPC();
+			}
 			
+			Npc npcToSpawn = PullFromPool();
+
+			Vector3 pos = new Vector3(basePos.x, basePos.y, basePos.z + i*1.5f);
+			npcToSpawn.gameObject.SetActive(true);
+			npcToSpawn.transform.SetPositionAndRotation(pos, Quaternion.identity);
+			npcToSpawn.stm.ChangeState(Npc.States.Wander);
+
+			yield return new WaitForSeconds(0.1f);
 		}
+	}
+
+	private Npc PullFromPool()
+	{
+		Npc npcToPull = _pooledNPCs.OrderBy(n => UnityEngine.Random.value).FirstOrDefault();
+		
+		// npcToPull.targetItem = RollTarget();
+
+		_pooledNPCs.Remove(npcToPull);
+
+		return npcToPull;
+	}
+
+	private void PushToPool(Npc npcToPush)
+	{
+		npcToPush.gameObject.SetActive(false);
+		_pooledNPCs.Add(npcToPush);
 	}
 
 	private void NewNPC()
@@ -27,7 +64,6 @@ public class CrowdManager : MonoBehaviour
 		Npc npcInstance = newNPC.GetComponent<Npc>(); 
 
 		npcInstance.info = RollInfo();
-		npcInstance.targetItem = RollTarget();
 
 		npcInstance.Init();
 
@@ -40,8 +76,8 @@ public class CrowdManager : MonoBehaviour
 		return infos[Random.Range(0, infos.Length)];
 	}
 
-	private Item RollTarget()
-	{
-		return items[Random.Range(0, items.Length)];
-	}
+	// private Item RollTarget()
+	// {
+	// 	return items[Random.Range(0, items.Length)];
+	// }
 }
--- Assets/_Script/Npc.cs
+++ Assets/_Script/Npc.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
+using MonsterLove.StateMachine;
 
 [RequireComponent(typeof(Rigidbody),typeof(NavMeshAgent))]
 public class Npc : MonoBehaviour 
@@ -10,15 +13,61 @@ public class Npc : MonoBehaviour
 
 	private NavMeshAgent _agent;
 	private Rigidbody _rb;
+	private Transform _modelAnchor;
+
+	public enum States
+	{
+		Wander,
+		Target,
+		KO
+	}
+	
+	public StateMachine<States> stm;
 
 	public void Init()
 	{
+		stm = StateMachine<States>.Initialize(this);
+		
 		_agent = GetComponent<NavMeshAgent>();
 		_rb = GetComponent<Rigidbody>();
+		_modelAnchor = transform.Find("model");
+
+		_agent.speed = info.moveSpeed;
+		Instantiate(info.model, _modelAnchor.position, Quaternion.identity, _modelAnchor);
+	}
+
+	private void Wander_Enter()
+	{
+		StartCoroutine(Wander());
+	}
+
+	private void Wander_Exit()
+	{
+		StopCoroutine(Wander());
 	}
 
 	public void GoTo(Vector3 pos)
 	{		
 		_agent.SetDestination(pos);
-	}	
+	}
+
+	private IEnumerator Wander()
+	{
+		yield return new WaitForSeconds(Random.Range(1f,4f));
+
+		GoTo(RandNavMeshPos(transform.position, 15));
+		StartCoroutine(Wander());
+	}
+
+	public Vector3 RandNavMeshPos(Vector3 origin, float dist)
+    {
+        Vector3 randDir = UnityEngine.Random.insideUnitSphere * dist;
+
+        randDir += origin;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randDir, out hit, dist, NavMesh.AllAreas);
+
+        return hit.position;
+    }
 }
